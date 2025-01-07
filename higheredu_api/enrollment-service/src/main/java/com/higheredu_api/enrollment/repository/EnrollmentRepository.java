410f5d57073075900bf90de3598370b8aa37a01f
Sarveswararao-coder <sarvesvararaos@gmail.com>
2025-01-07 14:58:13 +0530

    Delete higheredu_api directory

--- higheredu_api/enrollment-service/src/main/java/com/higheredu_api/enrollment/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.higheredu_api.enrollment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.higheredu_api.enrollment.model.Enrollment;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}

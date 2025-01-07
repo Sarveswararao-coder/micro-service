2f92749f47bfd41e01faf03b057aaa08e8170de5
Sarveswararao-coder <sarvesvararaos@gmail.com>
2025-01-07 14:24:58 +0530

    Delete monolithic directory

--- monolithic/src/main/java/com/higheredu/api/monolithic/repository/EnrollmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.higheredu.api.monolithic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.higheredu.api.monolithic.model.Enrollment;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}

8e3d249bf5ab9de49dc2dd87bc6791c099d30ed3
Sarveswararao-coder <sarvesvararaos@gmail.com>
2025-01-16 14:50:44 +0530

    Delete HigherEduApp directory

--- HigherEduApp/src/main/java/com/higheredu/app/repository/EnrollmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.higheredu.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.higheredu.app.model.Enrollment;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
